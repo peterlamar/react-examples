f06eff558ed267c76575cd523d518ba46891d1b7
Peter Lamar <peterrlamar@gmail.com>
2021-10-08 13:50:28 -0400

    Gatsby tutorial

---
+++ my-gatsby-site/src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Layout from '../components/layout'
+
+
+// markup
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby tutorial.</p>
+    </Layout>
+  )
+}
+
+export default IndexPage
