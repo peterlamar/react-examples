d6b7ae72755ca7867e351d913ea02089e373c590
Peter Lamar <peterrlamar@gmail.com>
2021-10-09 15:14:05 -0400

    Recording progress

--- my-gatsby-site/src/pages/index.js
+++ my-gatsby-site/src/pages/index.js
@@ -1,12 +1,16 @@
 import * as React from "react"
 import Layout from '../components/layout'
-
+import { StaticImage } from "gatsby-plugin-image"
 
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage
+        alt="Mantis Shrimp"
+        src="../images/mantis-shrimp.jpeg"
+      />
     </Layout>
   )
 }
